8b92c08dc3aa51dace683a3e36effe3742b6d727
Alonso <alonso.lai004@gmail.com>
2024-11-17 16:30:22 -0500

    stats.c now calculates avg, big, small. need to implement threading

--- Multithreaded_Statistical_Calculation/stats.c
+++ Multithreaded_Statistical_Calculation/stats.c
@@ -1,6 +1,28 @@
+#ifdef __STDC_NO_THREADS__
+    #error "This program requires C11 <threads.h> to build"
+#endif
+#include <threads.h>
 #include <stdio.h>
-#include <pthread.h>
+
+int largest_of_two_ints(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int smallest_of_two_ints(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+float average_of_two_ints(int a, int b) {
+    return (a + b)/2.0;
+}
 
 int main() {
-    printf("Hello World!\n");
+    int num1;
+    int num2;
+    printf("give two ints\n");
+    scanf("%d %d", &num1, &num2);
+    float average = average_of_two_ints(num1, num2);
+    int largets = largest_of_two_ints(num1, num2);
+    int smallest = smallest_of_two_ints(num1, num2);
+    printf("Average: %f\nLargest: %d\nSmallest: %d\n", average, largets, smallest);
 }
